c804d426c397f8b5ed538c06ae5e934be76558e6
Santiago Miqueo <smiqueo@hotmail.com>
2021-11-30 18:38:14 -0600

    Created Block component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <h1>Santi Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
 };
 </script>
 
